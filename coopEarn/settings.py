65fcfced62142a3b9fe0f08d1e4ab99795f7647a
Ankit <ankit971869@gmail.com>
2021-11-23 02:00:59 +0530

    Adding static root

--- coopEarn/settings.py
+++ coopEarn/settings.py
@@ -142,7 +142,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
