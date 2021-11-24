f31ab24740e762958e5d2955b0cf0e1811376a0a
Ankit <ankit971869@gmail.com>
2021-11-25 02:18:23 +0530

    imported cloudinary_storage module

--- coopEarn/settings.py
+++ coopEarn/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import cloudinary_storage
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
